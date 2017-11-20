04bd6e024d74cfddfa072c428eed7cc96daa0f40
maartenplieger <maarten.plieger@knmi.nl>
2017-11-20 14:55:34 +0100

    Updated adaguc-server docker with logrotate

--- adaguc-server-docker/start.sh
+++ adaguc-server-docker/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Starting POSTGRESQL DB" && \
-    runuser -l postgres -c "pg_ctl -w -D /postgresql -l /var/log/postgresql.log start" && \
+    runuser -l postgres -c "pg_ctl -D /postgresql -l /var/log/postgresql.log -w start" && \
+    sleep 1 && \
     cp /adaguc/adaguc-server-master/data/datasets/testdata.nc /data/adaguc-autowms && \
     cp /adaguc/adaguc-server-master/data/config/datasets/baselayers.xml /data/adaguc-datasets && \
+    cp /adaguc/adaguc-server-master/data/config/datasets/dataset_a.xml /data/adaguc-datasets && \
     echo "Configuring POSTGRESQL DB" && \
     runuser -l postgres -c "createuser --superuser adaguc" && \
     runuser -l postgres -c "psql postgres -c \"ALTER USER adaguc PASSWORD 'adaguc';\"" && \
