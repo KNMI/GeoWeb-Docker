81e8f80587290b9527db343da13802245f0f8191
maartenplieger <maarten.plieger@knmi.nl>
2017-11-20 17:16:42 +0100

    Updated Frontend with NGINX build. Configuration of backend URLS TBD

---
+++ geoweb-frontend-nginx-docker/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Building build env"
+
+docker build -t nodebuild . -f Dockerfile.nodebuild
+#
+docker create --name extract nodebuild
+docker cp extract:/frontend/geoweb-frontend/dist dist
+docker rm -f extract
+
+echo Building nginx runner
+docker build --no-cache -t geoweb-frontend-nginx . -f Dockerfile.geoweb-frontend-nginx
+rm -r dist
